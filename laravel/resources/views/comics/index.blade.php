e5235c37697537fe8e3e1ad68a088d3a3abd2565
AndreaDex <desiderio.andrea@outlook.it>
2021-07-14 16:15:21 +0200

    create Comic model, Comic controller, Comic migrat

---
+++ laravel/resources/views/comics/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('title', ' Comics')
+
+@section('content')
+
+<h1>Comics</h1>
+
+@endsection
---
+++ laravel/resources/views/comics/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('title', ' Comic')
+
+@section('content')
+
+<h1>Cominc</h1>
+
+@endsection
